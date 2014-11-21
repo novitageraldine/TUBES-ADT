0e839b4e5ec2f5f7c36d66f08fa071df6cf3e5ab
astarinatyasr <asnasa@yahoo.com>
2014-11-21 13:36:52 +0700

    Update ADT List.h

--- ADT List.h
+++ ADT List.h
@@ -1,1 +1,64 @@
-sddweqewqeqe
+
+#ifndef
+#define ADTList_H
+
+#include <stdio.h>
+#include "boolean.h"
+
+/*Konstanta*/
+#define Nil NULL
+
+typedef struct {
+    char KompleksG;
+    char NamaG[15];
+    long int HargaBeliG;
+    char PemilikG[20];
+}infotype;
+typedef struct tGedung *address;
+typedef struct tGedung{
+    infotype InfoG;
+    address NextG;
+} Gedung;
+
+/* Definisi list : */
+/* List kosong : First(L) = Nil */
+/* Setiap elemen dengan address P dapat diacu Info(P), Next(P) */
+/* Elemen terakhir list : jika addressnya Last,
+maka Next(Last) = First */
+typedef struct {
+    address First;
+} ListGedung;
+
+#endif // ADTList_H
+
+/*Selektor*/
+#define KompleksG(P) (P)->InfoG.KompleksG
+#define NamaG(P) (P)->InfoG.NamaG
+#define HargaBeliG(P) (P)->InfoG.HargaBeliG
+#define PemilikG(P) (P)->InfoG.PemilikG
+#define NextG(P) (P)->InfoG.NextG
+#define First(L) ((L).First)
+
+/* ***************** Pembuatan Papan Let's Get Rich ***************** */
+void CreateKGedung(ListGedung *L);
+/*I.S. Gedung terdefinisi */
+/*F.S. Kompleks gedung-gedung seperti sepsifikasi game terbentuk*/
+
+/* ***************** Manajemen Memori *****************/
+address Alokasi(infotype X);
+/* Mengirimkan address hasil alokasi sebuah elemen */
+/* Jika alokasi berhasil, maka address tidak Nil, dan misalnya menghasilkan P, maka
+Info(P) = X, Next(P) = Nil */
+/* Jika alokasi berhasil, maka address tidak Nil, dan misalnya menghasilkan P, maka
+Info(P) = X, Next(P) = Nil */
+
+void Dealokasi(address *P);
+/* I.S. P terdefinisi */
+/* F.S. P dikembalikan ke sistem */
+/* Melakukan dealokasi/pengembalian address P */
+
+/* ***************** PRIMITIF BERDASARKAN ALAMAT ***************** */
+/* *** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT *** */
+void InsertLast(List *L, address P);
+/* I.S. Sembarang, P sudah dialokasi */
+/* F.S. P ditambahkan sebagai elemen terakhir yang baru */
