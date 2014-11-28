8fe0e6de1b65ee75dbf096db20c22f222614e830
astarinatyasr <asnasa@yahoo.com>
2014-11-28 14:09:53 +0700

    Update ADT List.c

--- ADT List.c
+++ ADT List.c
@@ -1,123 +1,165 @@
-/*
-typedef char infotype[20];
-typedef struct tGedung *address;
-typedef struct tGedung{
-    infotype NamaG;
-    address NextG;
-} Gedung;
-*/
+
+#include "ADT List.h"
+
+/* ***************** TEST LIST KOSONG ***************** */
+boolean IsListEmpty(ListGedung LG)
+/* Mengirim true jika list kosong */
+{
+    return(First(LG)==Nil);
+}
 
 /* ***************** Pembuatan Papan Let's Get Rich ***************** */
-void CreateKGedung(ListGedung *L)
+void CreateKGedung(ListGedung * LG)
 /*I.S. Gedung terdefinisi */
 /*F.S. Kompleks gedung-gedung seperti sepsifikasi game terbentuk*/
 {
     /*Kamus Lokal*/
     infotype X;
+    address P;
     /*Algoritma*/
-    First(*L) = Nil; //Insisasi
+    First(*LG) = Nil; //Insisasi
     /*Alokasi START*/
     X.KompleksG = Nil;
     strcpy(X.NamaG, "START");
     X.HargaBeliG = Nil;
-    strcpy(X.PemilikG, Nil);
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Oktagon*/
     X.KompleksG = '1';
     strcpy(X.NamaG, "OKTAGON");
     X.HargaBeliG =500000;
-    strcpy(X.PemilikG, Nil);
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Comlabs*/
     X.KompleksG = '1';
     strcpy(X.NamaG, "COMLABS");
     X.HargaBeliG = 750000;
-    strcpy(X.PemilikG, Nil);
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi TVST*/
     X.KompleksG = '2';
     strcpy(X.NamaG, "TVST");
     X.HargaBeliG = 1200000;
-    strcpy(X.PemilikG, Nil);
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Telkom*/
     X.KompleksG = '2';
     strcpy(X.NamaG, "TELKOM");
     X.HargaBeliG = 1500000;
-    strcpy(X.PemilikG, Nil);
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Penjara*/
     X.KompleksG = Nil;
     strcpy(X.NamaG, "PENJARA");
     X.HargaBeliG = Nil;
-    strcpy(X.PemilikG, Nil);
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi K_Bengkok*/
     X.KompleksG = 'K';
-    strcpy(X, "K_BENGKOK");
+    strcpy(X.NamaG, "K_BENGKOK");
     X.HargaBeliG = 3500000;
-    X.PemilikG = Nil;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Kesempatan*/
-    strcpy(X, "KESEMPATAN");
+    X.KompleksG = Nil;
+    strcpy(X.NamaG, "KESEMPATAN");
+    X.HargaBeliG = Nil;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi GKU_Barat*/
-    strcpy(X, "GKU_BARAT");
+    X.KompleksG = '3';
+    strcpy(X.NamaG, "GKU_BARAT");
+    X.HargaBeliG = 1900000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi GKU_Timur*/
-    strcpy(X, "GKU_TIMUR");
+    X.KompleksG = '3';
+    strcpy(X.NamaG, "GKU_TIMUR");
+    X.HargaBeliG = 2300000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Undian*/
-    strcpy(X, "UNDIAN");
+    X.KompleksG = Nil;
+    strcpy(X.NamaG, "UNDIAN");
+    X.HargaBeliG = Nil;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Aula_Barat*/
-    strcpy(X, "AULA_BARAT");
+    X.KompleksG = '4';
+    strcpy(X.NamaG, "AULA_BARAT");
+    X.HargaBeliG = 2700000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Aula_Timur*/
-    strcpy(X, "AULA_TIMUR");
+    X.KompleksG = '4';
+    strcpy(X.NamaG, "AULA_TIMUR");
+    X.HargaBeliG = 3100000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Kesempatan*/
-    strcpy(X, "KESEMPATAN");
+    X.KompleksG = Nil;
+    strcpy(X.NamaG, "KESEMPATAN");
+    X.HargaBeliG = Nil;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi K_Barrack*/
-    strcpy(X, "K_BARRACK");
+    X.KompleksG = 'K';
+    strcpy(X.NamaG, "K_BARRACK");
+    X.HargaBeliG = 3500000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Bus_ITB*/
-    strcpy(X, "BUS_ITB");
+    X.KompleksG = Nil;
+    strcpy(X.NamaG, "BUS_ITB");
+    X.HargaBeliG = Nil;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Kesempatan*/
-    strcpy(X, "KESEMPATAN");
+    X.KompleksG = Nil;
+    strcpy(X.NamaG, "KESEMPATAN");
+    X.HargaBeliG = Nil;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi K_Borju*/
-    strcpy(X, "K_BORJU");
+    X.KompleksG = 'K';
+    strcpy(X.NamaG, "K_BORJU");
+    X.HargaBeliG = 3500000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Labtek_8*/
-    strcpy(X, "LABTEK_8");
+    X.KompleksG = '6';
+    strcpy(X.NamaG, "LABTEK_8");
+    X.HargaBeliG = 3500000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
+    InsertLast(&(*LG), P);
     /*Alokasi Lantek_6*/
-    strcpy(X, "LABTEK_6");
+    X.KompleksG = '6';
+    strcpy(X.NamaG, "LABTEK_6");
+    X.HargaBeliG = 4000000;
+    strcpy(X.PemilikG, " ");
     P = Alokasi(X);
-    InsertLast(L, P);
-    Next(P) = First(*L);
+    InsertLast(&(*LG), P);
+    NextG(P) = First(*LG);
+    printf("%s", NamaG(P));
 }
 
 /* ***************** Manajemen Memori *****************/
@@ -131,18 +173,17 @@ Info(P) = X, Next(P) = Nil */
     /*Kamus Lokal*/
     address P;
     /*Algoritma*/
-    P = malloc (sizeof(ElmtList));
+    P = malloc (sizeof(Gedung));
     if (P!=Nil) {
         KompleksG(P) = X.KompleksG;
-        strcpy(NamaG(P), X);
+        strcpy(NamaG(P), X.NamaG);
         HargaBeliG(P) = X.HargaBeliG;
         strcpy(PemilikG(P), X.PemilikG);
-        Next(P)=Nil;
+        NextG(P)=Nil;
         return(P);
     } else {
         return(Nil);
     }
-
 }
 
 void Dealokasi(address *P)
@@ -155,21 +196,47 @@ void Dealokasi(address *P)
 
 /* ***************** PRIMITIF BERDASARKAN ALAMAT ***************** */
 /* *** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT *** */
-void InsertLast(ListGedung *L, address P)
+void InsertLast(ListGedung *LG, address P)
 /* I.S. Sembarang, P sudah dialokasi */
 /* F.S. P ditambahkan sebagai elemen terakhir yang baru */
 {
      /*Kamus Lokal*/
     address Last;
     /*Algoritma*/
-    Last = First(*L);
-    if (IsListEmpty(*L)) {
-        Last = P;
+    Last = First(*LG);
+    if (IsListEmpty(*LG)) {
+        First(*LG) = P;
     } else {
-        while (Next(Last) != Nil) {
-            Last = Next(Last);
+        while (NextG(Last) != Nil) {
+            Last = NextG(Last);
         };
-        Next(P) = Nil;
-        Next(Last)= P;
+        NextG(P) = Nil;
+        NextG(Last)= P;
+    };
+}
+
+address Search(ListGedung *LG, char X[15])
+{
+    /*Kamus Lokal*/
+    address P;
+    boolean found;
+    /*Algoritma*/
+    P = First(*LG);
+    found = false;
+    if (P==Nil) { /* List kosong*/
+        return(Nil);
+    } else {
+        while ((P!=Nil) && (!found)) {
+            if (!strcmp(NamaG(P),X)) {
+                found = true;
+            } else {
+                P = NextG(P);
+            };
+        };
+        if (!strcmp(NamaG(P),X)) {
+            return(P);
+        } else {
+            return(Nil);
+        };
     };
 }
