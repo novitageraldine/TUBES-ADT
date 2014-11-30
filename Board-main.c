5c99e258d5e0f05721032c5ccce964399ead7e20
novitageraldine <novitageraldine@users.noreply.github.com>
2014-12-01 06:16:06 +0700

    Create Board-main.c

---
+++ Board-main.c
@@ -0,0 +1,104 @@
+#include "board.h"
+
+int main ()
+{
+    BuatBoard();
+    gotoxy(3,1);
+    printf("Board Permainan");
+    gotoxy(30,17);
+    printf("/$$                   / $$ \ ");
+    gotoxy(30,18);
+    printf("| $$                  | $$  |  \n");
+    gotoxy(30,19);
+    printf("| $$        /$$$$$$  /$$$$$$|_//$$$$$$$  \n");
+    gotoxy(30,20);
+    printf("| $$       /$$__  $$|_  $$_/  /$$_____/   \n ");
+    gotoxy(30,21);
+    printf("| $$      | $$$$$$$$  | $$   |  $$$$$$   \n");
+    gotoxy(30,22);
+    printf("| $$      | $$_____/  | $$ /$$\ ____  $$ \n  ");
+    gotoxy(30,23);
+    printf("| $$$$$$$$|  $$$$$$$  |  $$$$//$$$$$$$/  \n ");
+    gotoxy(30,24);
+    printf("|________/ \ _______/   \ ___/ |_______/   ");
+    gotoxy(35,27);
+    printf("/$$$$$$              /$$        \n");
+    gotoxy(35,28);
+    printf("/$$__  $$            | $$       \n");
+    gotoxy(35,29);
+    printf("| $$  \ __/  /$$$$$$  /$$$$$$   \n");
+    gotoxy(35,30);
+    printf("| $$ /$$$$ /$$__  $$|_  $$_/    \n");
+    gotoxy(35,31);
+    printf("| $$|_  $$| $$$$$$$$  | $$     \n");
+    gotoxy(35,32);
+    printf("| $$  \ $$| $$_____/  | $$ /$$ \n");
+    gotoxy(35,33);
+    printf("|  $$$$$$/|  $$$$$$$  |  $$$$/  \n");
+    gotoxy(35,34);
+    printf("\______/  \_______/   \___/    \n");
+
+    gotoxy(32,36);
+    printf(" /$$$$$$$ /$$           /$$     \n  ");
+    gotoxy(32,37);
+    printf("| $$__  $$|__/          | $$      \n ");
+    gotoxy(32,38);
+    printf("| $$  \ $$ /$$  /$$$$$$$| $$$$$$$  \n");
+    gotoxy(32,39);
+    printf("| $$$$$$$/| $$ /$$_____/| $$__  $$\n ");
+    gotoxy(32,40);
+    printf("| $$__  $$| $$| $$      | $$  \ $$\n ");
+    gotoxy(32,41);
+    printf("| $$  \ $$| $$| $$      | $$  | $$ \n");
+    gotoxy(32,42);
+    printf("| $$  | $$| $$|  $$$$$$$| $$  | $$ \n");
+    gotoxy(32,43);
+    printf("|__/  |__/|__/ \_______/|__/  |__/ \n ");
+
+    gotoxy(10,6);
+    printf("UNDIAN");
+    gotoxy(23,6);
+    printf("AULA_BARAT");
+    gotoxy(38,6);
+    printf("AULA_TIMUR");
+    gotoxy(53,6);
+    printf("KESEMPATAN");
+    gotoxy(69,6);
+    printf("K_BARRACK");
+    gotoxy(85,6);
+    printf("BUS_ITB");
+    gotoxy(83,15);
+    printf("KESEMPATAN");
+    gotoxy(85,24);
+    printf("K_BORJU");
+    gotoxy(83,33);
+    printf("LABTEK_VIII");
+    gotoxy(84,42);
+    printf("LABTEK_V");
+    gotoxy(86,51);
+    printf("START");
+    gotoxy(69,51);
+    printf("OKTAGON");
+    gotoxy(54,51);
+    printf("COMLABS");
+    gotoxy(40,51);
+    printf("TVST");
+    gotoxy(25,51);
+    printf("TELKOM");
+    gotoxy(10,51);
+    printf("UNDIAN");
+    gotoxy(9,15);
+    printf("GKU_TIMUR");
+    gotoxy(9,24);
+    printf("GKU_BARAT");
+    gotoxy(8,33);
+    printf("KESEMPATAN");
+    gotoxy(9,42);
+    printf("K_BENGKOK");
+    gotoxy(9,51);
+    printf("PENJARA");
+
+    gotoxy(0,0);
+
+}
+
